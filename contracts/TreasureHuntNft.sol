429b45b18a19ac9cf21f042c327bcb95d5bebdb4
akshatlumb <akshat6231@gmail.com>
2024-01-13 23:14:08 +0530

    updated contract logic

--- contracts/TreasureHuntNft.sol
+++ contracts/TreasureHuntNft.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
+error TreasureHuntNft__WrongAnswer();
 contract TreasureHuntNft is ERC721URIStorage{
     // We store the address to the correct answers in an enum
     // We store the coordinates of the correct answers in a mapping 
@@ -12,9 +13,10 @@ contract TreasureHuntNft is ERC721URIStorage{
     uint256 public s_tokenCounter;
     string[] internal s_locationTokenUris ;
     enum location {
-        location1,
-        location2,
-        location3
+        // Considering Sector17, Elante and Sukhna lake locations in chandigarh for this project
+        Sector17,
+        Elante,
+        SukhnaLake
     }
 
     event NftMinted(location newLocation,address minter);
@@ -27,7 +29,11 @@ contract TreasureHuntNft is ERC721URIStorage{
     
     mapping (uint256 => location) public pincodeToLocation;
 
-    function mintNft(uint256 pincode)public{
+    // mintNft takes 2 parameters, index of the riddle and pincode of the current location of the user
+    function mintNft(uint256 index, uint256 pincode)public{
+        if(location(index) != pincodeToLocation[pincode]){
+            revert TreasureHuntNft__WrongAnswer();
+        }
         location newLocation = pincodeToLocation[pincode];
         address owner = msg.sender;
         uint256 newtokenId = s_tokenCounter;
